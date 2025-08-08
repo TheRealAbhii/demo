e416ce2414c61d907cdac1703837e7331044db46
Abhishek Yadav <aabhishekyadavv@gmail.com>
2025-08-09 00:58:39 +0530

    dev: Add printf statement
    	below are following points
    	- something happen
    	- some time things not in control jkjkjkjjkj

--- factorial.c
+++ factorial.c
@@ -14,11 +14,12 @@ int main()
 
     printf("Enter a number: ");
     printf("Enter a good number: ");
+    printf("Testing one");
     printf("done!!");
     scanf("%d", &num);
 
     if (num < 0) {
-        printf("Factorial is not defined for negative numbers\n");
+        printf("Factorial is not defined for negative numbers \n");
     }
     else {
         printf("Factorial of %d is %d\n", num, factorial(num));
--- funStructPtr.c
+++ funStructPtr.c
@@ -21,13 +21,11 @@ void sub(int, int);
 int main()
 {
     // designated initializer method
-    calculator var = {.add = add, .subtract = sub};
-
+    calculator var = {.add  = add, .subtract = sub};
     var.add(5, 6);
     var.subtract(7, 4);
     return 0;
 }
 
 void add(int a, int b) { printf("addition result = %d", a + b); }
-
 void sub(int a, int b) { printf("subtraction result = %d", a - b); }
